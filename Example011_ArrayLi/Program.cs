c43c9e1c2bea1ad7b9ca9331aa35cc746a5ee330
NatalyaPs <n09051@yandex.ru>
2022-08-08 18:55:04 +0300

    add Example 011 ArrayLi

---
+++ Example011_ArrayLi/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection) // int[] collection - назвали так аргумент
+{
+   int Length = collection.Length;
+   int index = 0;
+   while (index < Length)   // цикл
+   {
+        collection[index] = new Random().Next(1, 10);
+        index++;   // index = index +1
+   }
+}
+                            // имя col -ч.б.различные аргументы назывались по-разному (Col, collection)
+void PrintArray(int[] col)  // void - это метод, который не возвращает значения.
+                            // тогда нельзя использовать return
+{
+    int count = col.Length;  // кол-во элементов обозначили
+    int position = 0;  // позицию не через индекс, а через текущую позицию
+    while (position < count)
+    {
+         Console.WriteLine(col[position]);
+         position++;
+    }
+
+}
+
+int[] arrey = new int[10]; // определяем массив с названием array
+                             // указываем, что в массиве будет 10 эл-тов
+                             // new int [10] означает "создай массив, в котором будет 10 эл-тов"
+FillArray(array);
+PrintArray(array);
